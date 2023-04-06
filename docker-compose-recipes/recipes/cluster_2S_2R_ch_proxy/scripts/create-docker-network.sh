4cc6200a1594c63f4d493f7635f24dfabf0810a8
nellicus <tony.bonuccelli@clickhouse.com>
2023-04-06 15:54:55 +0200

    remove networks unless required

--- docker-compose-recipes/recipes/cluster_2S_2R_ch_proxy/scripts/create-docker-network.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# run one off to create the network
-
-docker network create \
-  --driver=bridge \
-  --subnet=192.168.9.0/24 \
-  --gateway=192.168.9.254 \
-  cluster_2S_2R_ch_proxy
