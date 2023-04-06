7559f93e436b13f38b78e50c24fbd249ab54e4bb
nellicus <tony.bonuccelli@clickhouse.com>
2023-04-06 17:15:17 +0200

    ensure eof new line

--- docker-compose-recipes/recipes/ch-and-openldap/extra/clickhouse_roles.sql
+++ docker-compose-recipes/recipes/ch-and-openldap/extra/clickhouse_roles.sql
@@ -1,17 +1,6 @@
 CREATE ROLE clickhouse_administrators;
-
-GRANT
-SELECT
-    ON *.* TO clickhouse_administrators;
-
+GRANT SELECT ON *.* TO clickhouse_administrators;
 CREATE ROLE clickhouse_sales;
-
-GRANT
-SELECT
-    ON sales_db.* TO clickhouse_sales;
-
+GRANT SELECT ON sales_db.* TO clickhouse_sales;
 CREATE ROLE clickhouse_marketing;
-
-GRANT
-SELECT
-    ON marketing_db.* TO clickhouse_marketing;
+GRANT SELECT ON marketing_db.* TO clickhouse_marketing;
