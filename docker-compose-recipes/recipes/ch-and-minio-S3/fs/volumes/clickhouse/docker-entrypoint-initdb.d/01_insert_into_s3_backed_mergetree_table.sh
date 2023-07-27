dc61160c4501f9390bec3352ace5641b1045f551
nellicus <tony.bonuccelli@clickhouse.com>
2023-07-27 09:59:45 +0200

    use s3 backed table, replace deprecated envvarss

---
+++ docker-compose-recipes/recipes/ch-and-minio-S3/fs/volumes/clickhouse/docker-entrypoint-initdb.d/01_insert_into_s3_backed_mergetree_table.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+clickhouse client -n <<-EOSQL
+INSERT INTO trips_s3 SELECT trip_id, pickup_date, pickup_datetime, dropoff_datetime, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, passenger_count, trip_distance, tip_amount, total_amount, payment_type FROM s3('https://ch-nyc-taxi.s3.eu-west-3.amazonaws.com/tsv/trips_{0..9}.tsv.gz','TabSeparatedWithNames') LIMIT 1000000;
+EOSQL
