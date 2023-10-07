1f9f1a49cfb0d12a212f1e8a184de8ff5f3077b4
tom-clickhouse <97666923+tom-clickhouse@users.noreply.github.com>
2023-10-07 18:53:13 +0200

    Update load_files.py

--- large_data_loads/src/load_files.py
+++ large_data_loads/src/load_files.py
@@ -147,8 +147,7 @@ def get_file_urls_and_row_counts(url, configuration, client):
         concat(_protocol, '://', _domain,  if(startsWith(_path, '/') , '', '/'), _path) as file,
         count() as count
     FROM {function_fragment}'{url}'{format_fragment}) {configuration['where']}
-    GROUP BY 1
-    {settings_fragment}"""
+    GROUP BY 1"""
 
     logger.debug(f"Query for full path urls and row counts:{query}")
 
