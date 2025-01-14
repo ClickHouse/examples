614bb6d8c212584501efe0306da365d86f54b917
Tom Schreiber <tom@clickhouse.com>
2025-01-14 13:05:29 +0000

    Track memory usage for ClickHouse + some refactorings.

--- ClickJSONBench/clickhouse/main.sh
+++ ClickJSONBench/clickhouse/main.sh
@@ -26,39 +26,39 @@ chmod +x ./*.sh
 # bluesky_1m_lz4
 ./create_and_load.sh bluesky_1m_lz4 bluesky ddl_lz4.sql "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
 ./data_size.sh bluesky_1m_lz4 bluesky | tee "${OUTPUT_PREFIX}_bluesky_1m_lz4.data_size"
-./benchmark.sh bluesky_1m_lz4 "${OUTPUT_PREFIX}_bluesky_1m_lz4.result"
+./benchmark.sh bluesky_1m_lz4 "${OUTPUT_PREFIX}_bluesky_1m_lz4.results_runtime" "${OUTPUT_PREFIX}_bluesky_1m_lz4.results_memory_usage"
 
 # bluesky_10m_lz4
 ./create_and_load.sh bluesky_10m_lz4 bluesky ddl_lz4.sql "$DATA_DIRECTORY" 10 "$SUCCESS_LOG" "$ERROR_LOG"
 ./data_size.sh bluesky_10m_lz4 bluesky | tee "${OUTPUT_PREFIX}_bluesky_10m_lz4.data_size"
-./benchmark.sh bluesky_10m_lz4 "${OUTPUT_PREFIX}_bluesky_10m_lz4.result"
+./benchmark.sh bluesky_10m_lz4 "${OUTPUT_PREFIX}_bluesky_10m_lz4.results_runtime" "${OUTPUT_PREFIX}_bluesky_10m_lz4.results_memory_usage"
 
 # bluesky_100m_lz4
 ./create_and_load.sh bluesky_100m_lz4 bluesky ddl_lz4.sql "$DATA_DIRECTORY" 100 "$SUCCESS_LOG" "$ERROR_LOG"
 ./data_size.sh bluesky_100m_lz4 bluesky | tee "${OUTPUT_PREFIX}_bluesky_100m_lz4.data_size"
-./benchmark.sh bluesky_100m_lz4 "${OUTPUT_PREFIX}_bluesky_100m_lz4.result"
+./benchmark.sh bluesky_100m_lz4 "${OUTPUT_PREFIX}_bluesky_100m_lz4.results_runtime" "${OUTPUT_PREFIX}_bluesky_100m_lz4.results_memory_usage"
 
 # bluesky_1000m_lz4
 ./create_and_load.sh bluesky_1000m_lz4 bluesky ddl_lz4.sql "$DATA_DIRECTORY" 1000 "$SUCCESS_LOG" "$ERROR_LOG"
 ./data_size.sh bluesky_1000m_lz4 bluesky | tee "${OUTPUT_PREFIX}_bluesky_1000m_lz4.data_size"
-./benchmark.sh bluesky_1000m_lz4 "${OUTPUT_PREFIX}_bluesky_1000m_lz4.result"
+./benchmark.sh bluesky_1000m_lz4 "${OUTPUT_PREFIX}_bluesky_1000m_lz4.results_runtime" "${OUTPUT_PREFIX}_bluesky_1000m_lz4.results_memory_usage"
 
 # bluesky_1m_zstd
 ./create_and_load.sh bluesky_1m_zstd bluesky ddl_zstd.sql "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
 ./data_size.sh bluesky_1m_zstd bluesky | tee "${OUTPUT_PREFIX}_bluesky_1m_zstd.data_size"
-./benchmark.sh bluesky_1m_zstd "${OUTPUT_PREFIX}_bluesky_1m_zstd.result"
+./benchmark.sh bluesky_1m_zstd "${OUTPUT_PREFIX}_bluesky_1m_zstd.results_runtime" "${OUTPUT_PREFIX}_bluesky_1m_zstd.results_memory_usage"
 
 # bluesky_10m_zstd
 ./create_and_load.sh bluesky_10m_zstd bluesky ddl_zstd.sql "$DATA_DIRECTORY" 10 "$SUCCESS_LOG" "$ERROR_LOG"
 ./data_size.sh bluesky_10m_zstd bluesky | tee "${OUTPUT_PREFIX}_bluesky_10m_zstd.data_size"
-./benchmark.sh bluesky_10m_zstd "${OUTPUT_PREFIX}_bluesky_10m_zstd.result"
+./benchmark.sh bluesky_10m_zstd "${OUTPUT_PREFIX}_bluesky_10m_zstd.results_runtime" "${OUTPUT_PREFIX}_bluesky_10m_zstd.results_memory_usage"
 
 # bluesky_100m_zstd
 ./create_and_load.sh bluesky_100m_zstd bluesky ddl_zstd.sql "$DATA_DIRECTORY" 100 "$SUCCESS_LOG" "$ERROR_LOG"
 ./data_size.sh bluesky_100m_zstd bluesky | tee "${OUTPUT_PREFIX}_bluesky_100m_zstd.data_size"
-./benchmark.sh bluesky_100m_zstd "${OUTPUT_PREFIX}_bluesky_100m_zstd.result"
+./benchmark.sh bluesky_100m_zstd "${OUTPUT_PREFIX}_bluesky_100m_zstd.results_runtime" "${OUTPUT_PREFIX}_bluesky_100m_zstd.results_memory_usage"
 
 # bluesky_1000m_zstd
 ./create_and_load.sh bluesky_1000m_zstd bluesky ddl_zstd.sql "$DATA_DIRECTORY" 1000 "$SUCCESS_LOG" "$ERROR_LOG"
 ./data_size.sh bluesky_1000m_zstd bluesky | tee "${OUTPUT_PREFIX}_bluesky_1000m_zstd.data_size"
-./benchmark.sh bluesky_1000m_zstd "${OUTPUT_PREFIX}_bluesky_1000m_zstd.result"
+./benchmark.sh bluesky_1000m_zstd "${OUTPUT_PREFIX}_bluesky_1000m_zstd.results_runtime" "${OUTPUT_PREFIX}_bluesky_1000m_zstd.results_memory_usage"
