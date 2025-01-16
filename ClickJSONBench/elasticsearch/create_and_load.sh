7aa7ba69f67626908956cc294e4befebb85b5f7a
Lionel Palacin <lionel.palacin@clickhouse.com>
2025-01-16 16:31:56 +0000

    add filebeat ingestion

--- ClickJSONBench/elasticsearch/create_and_load.sh
+++ ClickJSONBench/elasticsearch/create_and_load.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+# Check if ELASTIC_PASSWORD env variable is set, if set from not read from .elastic_password file
+if [[ -z "$ELASTIC_PASSWORD" ]]; then
+    [[ ! -f ".elastic_password" ]] && { echo "Error: ELASTIC_PASSWORD environment variable is not set and .elastic_password file does not exist."; exit 1; }
+    export $(cat .elastic_password)
+fi
+
 # Check if the required arguments are provided
-if [[ $# -lt 7 ]]; then
+if [[ $# -lt 6 ]]; then
     echo "Usage: $0 <INDEX_NAME> <INDEX_TEMPLATE_FILE> <DATA_DIRECTORY> <NUM_FILES> <SUCCESS_LOG> <ERROR_LOG>"
     exit 1
 fi
 
 # Arguments
 INDEX_NAME="$1"
-INDEX_TEMPLATE_FILE="config/$2"
+INDEX_TEMPLATE_FILE="config/$2.json"
 DATA_DIRECTORY="$3"
 NUM_FILES="$4"
 SUCCESS_LOG="$5"
@@ -19,12 +25,28 @@ ERROR_LOG="$6"
 [[ ! -d "$DATA_DIRECTORY" ]] && { echo "Error: Data directory '$DATA_DIRECTORY' does not exist."; exit 1; }
 [[ ! "$NUM_FILES" =~ ^[0-9]+$ ]] && { echo "Error: NUM_FILES must be a positive integer."; exit 1; }
 
+# Check ilm policy is installed, install if not
+# If curl return 404, means ILM policy is not installed
+
+http_code=$(curl -s -o /dev/null -k -w "%{http_code}" -X GET "https://localhost:9200/_ilm/policy/filebeat" -u "elastic:${ELASTIC_PASSWORD}" -H 'Content-Type: application/json')
+if [[ "$http_code" -eq 404 ]] ; then
+    echo "Installing ILM policy"
+    ILM_POLICY=$(cat "config/ilm.json")
+    curl -s -k -X PUT "https://localhost:9200/_ilm/policy/filebeat" -u "elastic:${ELASTIC_PASSWORD}" -H 'Content-Type: application/json' -d "$ILM_POLICY"
+fi
+
 # Install index template
 # Read index template file json from config/$INDEX_TEMPLATE_FILE 
 INDEX_TEMPLATE=$(cat "$INDEX_TEMPLATE_FILE")
-eval "INDEX_TEMPLATE=\"${INDEX_TEMPLATE}\""
-echo $INDEX_TEMPLATE
+JSON_DATA=$(cat $INDEX_TEMPLATE_FILE | sed "s/\${INDEX_NAME}/$INDEX_NAME/g")
+echo "Install index template"
+curl -s -o /dev/null -k -X PUT "https://localhost:9200/_index_template/${INDEX_NAME}" -u "elastic:${ELASTIC_PASSWORD}" -H 'Content-Type: application/json' -d "$JSON_DATA"
+
+# Create the data stream
+echo "Create the data stream"
+curl -s -o /dev/null -k -X PUT "https://localhost:9200/_data_stream/${INDEX_NAME}" -u "elastic:${ELASTIC_PASSWORD}" -H 'Content-Type: application/json'
+
 # Load data
-#./load_data.sh "$DATA_DIRECTORY" "$DB_NAME" "$COLLECTION_NAME" "$NUM_FILES" "$SUCCESS_LOG" "$ERROR_LOG"
+./load_data.sh "$DATA_DIRECTORY" "$INDEX_NAME" "$NUM_FILES" "$SUCCESS_LOG" "$ERROR_LOG"
 
 echo "Script completed successfully."
--- ClickJSONBench/elasticsearch/load_data.sh
+++ ClickJSONBench/elasticsearch/load_data.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
 # Check if the required arguments are provided
-if [[ $# -lt 6 ]]; then
-    echo "Usage: $0 <directory> <database_name> <collection_name> <max_files> <success_log> <error_log>"
+if [[ $# -lt 5 ]]; then
+    echo "Usage: $0 <directory> <index_name> <max_files> <success_log> <error_log>"
     exit 1
 fi
 
 # Arguments
 DIRECTORY="$1"
-DB_NAME="$2"
-COLLECTION_NAME="$3"
-MAX_FILES="$4"
-SUCCESS_LOG="$5"
-ERROR_LOG="$6"
-MONGO_URI="mongodb://localhost:27017"   # Replace with your MongoDB URI if necessary
+INDEX_NAME="$2"
+MAX_FILES="$3"
+SUCCESS_LOG="$4"
+ERROR_LOG="$5"
 
 # Validate that MAX_FILES is a number
 if ! [[ "$MAX_FILES" =~ ^[0-9]+$ ]]; then
@@ -24,20 +22,22 @@ fi
 # Ensure the log files exist
 touch "$SUCCESS_LOG" "$ERROR_LOG"
 
-# Create a temporary directory for uncompressed files
-TEMP_DIR=$(mktemp -d /var/tmp/json_files.XXXXXX)
+# Create a temporary directory in /var/tmp and ensure it's accessible
+TEMP_DIR=$(mktemp -d /var/tmp/cleaned_files.XXXXXX)
+chmod 777 "$TEMP_DIR"  # Allow access for all users
 trap "rm -rf $TEMP_DIR" EXIT  # Ensure cleanup on script exit
 
+# Copy all files to temp location and uncompress them
 # Counter to track processed files
 counter=0
 
 # Loop through each .json.gz file in the directory
-for file in $(ls "$DIRECTORY"/*.json.gz 2>/dev/null | sort); do
+for file in $(ls "$DIRECTORY"/*.json.gz | sort); do
     if [[ -f "$file" ]]; then
         echo "Processing $file..."
         counter=$((counter + 1))
 
-        # Uncompress the file into the TEMP_DIR
+        # Uncompress the file into the temporary directory
         uncompressed_file="$TEMP_DIR/$(basename "${file%.gz}")"
         gunzip -c "$file" > "$uncompressed_file"
 
@@ -47,30 +47,41 @@ for file in $(ls "$DIRECTORY"/*.json.gz 2>/dev/null | sort); do
             continue
         fi
 
-        # Import the uncompressed JSON file into MongoDB
-        mongoimport --uri "$MONGO_URI" --db "$DB_NAME" --collection "$COLLECTION_NAME" --file "$uncompressed_file"
-        import_status=$?
+        # Preprocess the file to remove null characters
+        cleaned_file="$TEMP_DIR/$(basename "${uncompressed_file%.json}_cleaned.json")"
+        sed 's/\\u0000//g' "$uncompressed_file" > "$cleaned_file"
 
-        # Check if the import was successful
-        if [[ $import_status -eq 0 ]]; then
-            echo "[$(date '+%Y-%m-%d %H:%M:%S')] Successfully imported $uncompressed_file into MongoDB." >> "$SUCCESS_LOG"
-        else
-            echo "[$(date '+%Y-%m-%d %H:%M:%S')] Failed to import $uncompressed_file into MongoDB." >> "$ERROR_LOG"
-        fi
-
-        # Remove the uncompressed file after processing
-        rm -f "$uncompressed_file"
-
+        # Grant read permissions for the postgres user
+        chmod 644 "$cleaned_file"
         # Stop processing if the max number of files is reached
         if [[ $counter -ge $MAX_FILES ]]; then
             echo "Processed maximum number of files: $MAX_FILES"
             break
         fi
+    else
+        echo "No .json.gz files found in the directory."
     fi
 done
 
-if [[ $counter -eq 0 ]]; then
-    echo "No .json.gz files found in the directory."
-fi
+echo "All files have been copied to temp location."
+
+echo "Prepare fileebeat for ingestion"
+
+# Prepare Filebeat configuration
+FILEBEAT_API_KEY=$(cat .filebeat_api_key)
+FILEBEAT_CONFIG=$(cat "config/filebeat.yml" | sed "s/<api_key>/$FILEBEAT_API_KEY/g" | sed "s/<index_name>/$INDEX_NAME/g" | sed "s/<temp_dir>/$TEMP_DIR/g")
+echo "$FILEBEAT_CONFIG" | sudo tee /etc/filebeat/filebeat.yml > /dev/null
+
+sudo service start filebeat.service
+trap "sudo service stop filebeat.service" EXIT  # Stop filebeat on exit
+
+# wait until all files have been ingested 
+total_processed=0
+max_events=$MAX_FILES*1000000
+while total_processed -lt $max_events; do
+    total_processed=$(curl -k -s -XGET 'localhost:5066/stats' | jq '.beat.libbeat.output.events.total')
+    echo "Total processed files: $total_processed"
+    sleep 60
+done
 
 echo "All files have been processed."
