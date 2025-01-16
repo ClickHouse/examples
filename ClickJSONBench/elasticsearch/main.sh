c367102d78201ca8c5bc329ac2fc9b4b7838be1a
Lionel Palacin <lionel.palacin@clickhouse.com>
2025-01-16 18:36:13 +0000

    fix missing index template parameter

--- ClickJSONBench/elasticsearch/main.sh
+++ ClickJSONBench/elasticsearch/main.sh
@@ -27,76 +27,76 @@ fi
 # ./benchmark.sh bluesky-source-1m-best-compression "${OUTPUT_PREFIX}_bluesky-source-1m-best-compression.results_runtime"
 
 # bluesky-source-10m-best-compression
-./create_and_load.sh bluesky-source-10m-best-compression  "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
+./create_and_load.sh bluesky-source-10m-best-compression index_template_source_best_compression "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
 # ./total_size.sh bluesky-source-10m-best-compression | tee "${OUTPUT_PREFIX}_bluesky-source-10m-best-compression.data_size"
 # ./benchmark.sh bluesky-source-10m-best-compression "${OUTPUT_PREFIX}_bluesky-source-10m-best-compression.results_runtime"
 
 # bluesky-source-100m-best-compression
-./create_and_load.sh bluesky-source-100m-best-compression  "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
+./create_and_load.sh bluesky-source-100m-best-compression index_template_source_best_compression "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
 # ./total_size.sh bluesky-source-100m-best-compression | tee "${OUTPUT_PREFIX}_bluesky-source-100m-best-compression.data_size"
 # ./benchmark.sh bluesky-source-100m-best-compression "${OUTPUT_PREFIX}_bluesky-source-100m-best-compression.results_runtime"
 
 # # bluesky-source-1000m-best-compression
-# ./create_and_load.sh bluesky-source-1000m-best-compression  "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
+# ./create_and_load.sh bluesky-source-1000m-best-compression index_template_source_best_compression "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
 # ./total_size.sh bluesky-source-1000m-best-compression | tee "${OUTPUT_PREFIX}_bluesky-source-1000m-best-compression.data_size"
 # ./benchmark.sh bluesky-source-1000m-best-compression "${OUTPUT_PREFIX}_bluesky-source-1000m-best-compression.results_runtime"
 
 # # bluesky-source-1m-default-compression
-./create_and_load.sh bluesky-source-1m-default-compression  "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
+./create_and_load.sh bluesky-source-1m-default-compression index_template_source_default_compression "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
 # ./total_size.sh bluesky-source-1m-default-compression | tee "${OUTPUT_PREFIX}_bluesky-source-1m-default-compression.data_size"
 # ./benchmark.sh bluesky-source-1m-default-compression "${OUTPUT_PREFIX}_bluesky-source-1m-default-compression.results_runtime"
 
 # # bluesky-source-10m-default-compression
-./create_and_load.sh bluesky-source-10m-default-compression  "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
+./create_and_load.sh bluesky-source-10m-default-compression index_template_source_default_compression "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
 # ./total_size.sh bluesky-source-10m-default-compression | tee "${OUTPUT_PREFIX}_bluesky-source-10m-default-compression.data_size"
 # ./benchmark.sh bluesky-source-10m-default-compression "${OUTPUT_PREFIX}_bluesky-source-10m-default-compression.results_runtime"
 
 # # bluesky-source-100m-default-compression
-./create_and_load.sh bluesky-source-100m-default-compression  "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
+./create_and_load.sh bluesky-source-100m-default-compression index_template_source_default_compression "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
 # ./total_size.sh bluesky-source-100m-default-compression | tee "${OUTPUT_PREFIX}_bluesky-source-100m-default-compression.data_size"
 # ./benchmark.sh bluesky-source-100m-default-compression "${OUTPUT_PREFIX}_bluesky-source-100m-default-compression.results_runtime"
 
 # # bluesky-source-1000m-default-compression
-# ./create_and_load.sh bluesky-source-1000m-default-compression  "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
+# ./create_and_load.sh bluesky-source-1000m-default-compression index_template_source_default_compression "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
 # ./total_size.sh bluesky-source-1000m-default-compression | tee "${OUTPUT_PREFIX}_bluesky-source-1000m-default-compression.data_size"
 # ./benchmark.sh bluesky-source-1000m-default-compression "${OUTPUT_PREFIX}_bluesky-source-1000m-default-compression.results_runtime"
 
 # # bluesky-no-source-1m-default-compression
-./create_and_load.sh bluesky-no-source-1m-default-compression  "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
+./create_and_load.sh bluesky-no-source-1m-default-compression index_template_no_source_default_compression "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
 # ./total_size.sh bluesky-no-source-1m-default-compression | tee "${OUTPUT_PREFIX}_bluesky-no-source-1m-default-compression.data_size"
 # ./benchmark.sh bluesky-no-source-1m-default-compression "${OUTPUT_PREFIX}_bluesky-no-source-1m-default-compression.results_runtime"
 
 # # bluesky-no-source-10m-default-compression
-./create_and_load.sh bluesky-no-source-10m-default-compression  "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
+./create_and_load.sh bluesky-no-source-10m-default-compression index_template_no_source_default_compression "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
 # ./total_size.sh bluesky-no-source-10m-default-compression | tee "${OUTPUT_PREFIX}_bluesky-no-source-10m-default-compression.data_size"
 # ./benchmark.sh bluesky-no-source-10m-default-compression "${OUTPUT_PREFIX}_bluesky-no-source-10m-default-compression.results_runtime"
 
 # # bluesky-no-source-100m-default-compression
-./create_and_load.sh bluesky-no-source-100m-default-compression  "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
+./create_and_load.sh bluesky-no-source-100m-default-compression index_template_no_source_default_compression "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
 # ./total_size.sh bluesky-no-source-100m-default-compression | tee "${OUTPUT_PREFIX}_bluesky-no-source-100m-default-compression.data_size"
 # ./benchmark.sh bluesky-no-source-100m-default-compression "${OUTPUT_PREFIX}_bluesky-no-source-100m-default-compression.results_runtime"
 
 # # bluesky-no-source-1000m-default-compression
-# ./create_and_load.sh bluesky-no-source-1000m-default-compression  "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
+# ./create_and_load.sh bluesky-no-source-1000m-default-compression index_template_no_source_default_compression "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
 # ./total_size.sh bluesky-no-source-1000m-default-compression | tee "${OUTPUT_PREFIX}_bluesky-no-source-1000m-default-compression.data_size"
 # ./benchmark.sh bluesky-no-source-1000m-default-compression "${OUTPUT_PREFIX}_bluesky-no-source-1000m-default-compression.results_runtime"
 
 # # bluesky-no-source-1m-best-compression
-./create_and_load.sh bluesky-source-1m-best-compression  "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
+./create_and_load.sh bluesky-source-1m-best-compression index_template_no_source_best_compression "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
 # ./total_size.sh bluesky-no-source-1m-best-compression | tee "${OUTPUT_PREFIX}_bluesky-no-source-1m-best-compression.data_size"
 # ./benchmark.sh bluesky-no-source-1m-best-compression "${OUTPUT_PREFIX}_bluesky-no-source-1m-best-compression.results_runtime"
 
 # # bluesky-no-source-10m-best-compression
-./create_and_load.sh bluesky-source-10m-best-compression  "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
+./create_and_load.sh bluesky-source-10m-best-compression index_template_no_source_best_compression "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
 # ./total_size.sh bluesky-no-source-10m-best-compression | tee "${OUTPUT_PREFIX}_bluesky-no-source-10m-best-compression.data_size"
 # ./benchmark.sh bluesky-no-source-10m-best-compression "${OUTPUT_PREFIX}_bluesky-no-source-10m-best-compression.results_runtime"
 
 # # bluesky-no-source-100m-best-compression
-./create_and_load.sh bluesky-no-source-100m-best-compression  "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
+./create_and_load.sh bluesky-no-source-100m-best-compression index_template_no_source_best_compression "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
 # ./total_size.sh bluesky-no-source-100m-best-compression | tee "${OUTPUT_PREFIX}_bluesky-no-source-100m-best-compression.data_size"
 # ./benchmark.sh bluesky-no-source-100m-best-compression "${OUTPUT_PREFIX}_bluesky-no-source-100m-best-compression.results_runtime"
 
 # # bluesky-no-source-1000m-best-compression
-# #./create_and_load.sh bluesky-no-source-1000m-best-compression  "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
+# #./create_and_load.sh bluesky-no-source-1000m-best-compression index_template_no_source_best_compression "$DATA_DIRECTORY" 1 "$SUCCESS_LOG" "$ERROR_LOG"
 # ./total_size.sh bluesky-no-source-1000m-best-compression | tee "${OUTPUT_PREFIX}_bluesky-no-source-1000m-best-compression.data_size"
 # ./benchmark.sh bluesky-no-source-1000m-best-compression "${OUTPUT_PREFIX}_bluesky-no-source-1000m-best-compression.results_runtime"
