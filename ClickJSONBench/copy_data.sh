0242dc7ce5e68d33519e7b830213c99e20f85ecf
Lionel Palacin <lionel.palacin@clickhouse.com>
2025-01-20 12:09:56 +0000

    enhance copy data script

--- ClickJSONBench/copy_data.sh
+++ ClickJSONBench/copy_data.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
-# Uncomment one of the wget command to download the dataset of your choice - By default, the 1m dataset is downloaded
+echo "Select the dataset size to download:"
+echo "1) 1m (default)"
+echo "2) 10m"
+echo "3) 100m"
+echo "4) 1000m"
+read -p "Enter the number corresponding to your choice: " choice
 
-# Download 1m dataset
-wget https://clickhouse-public-datasets.s3.amazonaws.com/bluesky/file_0001.json.gz -P ~/data/bluesky
-
-# Download 10m dataset
-#wget https://clickhouse-public-datasets.s3.amazonaws.com/bluesky/file_{0001..0010}.json.gz -P ~/data/bluesky
-
-# Download 100m dataset
-#wget https://clickhouse-public-datasets.s3.amazonaws.com/bluesky/file_{0001..0100}.json.gz -P ~/data/bluesky
-
-# Download 1000m dataset
-#wget https://clickhouse-public-datasets.s3.amazonaws.com/bluesky/file_{0001..1000}.json.gz -P ~/data/bluesky
+case $choice in
+    2)
+        # Download 10m dataset
+        wget https://clickhouse-public-datasets.s3.amazonaws.com/bluesky/file_{0001..0010}.json.gz -P ~/data/bluesky
+        ;;
+    3)
+        # Download 100m dataset
+        wget https://clickhouse-public-datasets.s3.amazonaws.com/bluesky/file_{0001..0100}.json.gz -P ~/data/bluesky
+        ;;
+    4)
+        # Download 1000m dataset
+        wget https://clickhouse-public-datasets.s3.amazonaws.com/bluesky/file_{0001..1000}.json.gz -P ~/data/bluesky
+        ;;
+    *)
+        # Download 1m dataset
+        wget https://clickhouse-public-datasets.s3.amazonaws.com/bluesky/file_0001.json.gz -P ~/data/bluesky
+        ;;
+esac
