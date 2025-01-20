58b7ab6d5842abafcb6449664de1e6aaf19a34e0
Lionel Palacin <lionel.palacin@clickhouse.com>
2025-01-20 15:51:30 +0000

    uncomment es install script

--- ClickJSONBench/elasticsearch/install.sh
+++ ClickJSONBench/elasticsearch/install.sh
@@ -1,19 +1,19 @@
 # Install elasticsearch
-# wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
-# sudo apt-get install apt-transport-https
-# echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
-# sudo apt-get update && sudo apt-get install elasticsearch
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
+sudo apt-get install apt-transport-https
+echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
+sudo apt-get update && sudo apt-get install elasticsearch
 
 # # Install filebeat
-# curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-8.17.0-amd64.deb
-# sudo dpkg -i filebeat-8.17.0-amd64.deb
+curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-8.17.0-amd64.deb
+sudo dpkg -i filebeat-8.17.0-amd64.deb
 
 # # Overwrite configuration files
-# sudo cp config/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
-# sudo cp config/jvm.options /etc/elasticsearch/jvm.options
+sudo cp config/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
+sudo cp config/jvm.options /etc/elasticsearch/jvm.options
 
 # # Start elasticsearch
-# sudo systemctl start elasticsearch.service
+sudo systemctl start elasticsearch.service
 
 # Reset and export elastic password
 export ELASTIC_PASSWORD=$(sudo /usr/share/elasticsearch/bin/elasticsearch-reset-password -s -a -b -u elastic)
