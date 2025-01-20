ccf7f2a04b6ce63ee17080c07277194841aad1ed
Lionel Palacin <lionel.palacin@clickhouse.com>
2025-01-20 16:01:56 +0000

    read es password from local file

--- ClickJSONBench/elasticsearch/total_size.sh
+++ ClickJSONBench/elasticsearch/total_size.sh
@@ -6,6 +6,12 @@ if [[ $# -lt 1 ]]; then
     exit 1
 fi
 
+# Check if ELASTIC_PASSWORD env variable is set, if set from not read from .elastic_password file
+if [[ -z "$ELASTIC_PASSWORD" ]]; then
+    [[ ! -f ".elastic_password" ]] && { echo "Error: ELASTIC_PASSWORD environment variable is not set and .elastic_password file does not exist."; exit 1; }
+    export $(cat .elastic_password)
+fi
+
 # Arguments
 INDEX_NAME="$1"
 
