ab2843df01b90d9c6ff5cbf359fac0fa6376ed93
Tom Schreiber <tom@clickhouse.com>
2025-01-22 07:06:58 +0000

    Renumber queries.

--- ClickJSONBench/clickhouse/queries_formatted.sql
+++ ClickJSONBench/clickhouse/queries_formatted.sql
@@ -1,5 +1,5 @@
 ------------------------------------------------------------------------------------------------------------------------
--- Q0 - Top event types
+-- Q1 - Top event types
 ------------------------------------------------------------------------------------------------------------------------
 SELECT
     data.commit.collection AS event,
@@ -9,7 +9,7 @@ GROUP BY event
 ORDER BY count DESC;
 
 ------------------------------------------------------------------------------------------------------------------------
--- Q1 - Top event types together with unique users per event type
+-- Q2 - Top event types together with unique users per event type
 ------------------------------------------------------------------------------------------------------------------------
 SELECT
     data.commit.collection AS event,
@@ -22,7 +22,7 @@ GROUP BY event
 ORDER BY count DESC;
 
 ------------------------------------------------------------------------------------------------------------------------
--- Q2 - When do people use BlueSky
+-- Q3 - When do people use BlueSky
 ------------------------------------------------------------------------------------------------------------------------
 SELECT
     data.commit.collection AS event,
@@ -36,7 +36,7 @@ GROUP BY event, hour_of_day
 ORDER BY hour_of_day, event;
 
 ------------------------------------------------------------------------------------------------------------------------
--- Q3 - top 3 post veterans
+-- Q4 - top 3 post veterans
 ------------------------------------------------------------------------------------------------------------------------
 SELECT
     data.did::String as user_id,
@@ -50,7 +50,7 @@ ORDER BY first_post_ts ASC
 LIMIT 3;
 
 ------------------------------------------------------------------------------------------------------------------------
--- Q4 - top 3 users with longest activity
+-- Q5 - top 3 users with longest activity
 ------------------------------------------------------------------------------------------------------------------------
 SELECT
     data.did::String as user_id,
@@ -64,34 +64,4 @@ WHERE data.kind = 'commit'
   AND data.commit.collection = 'app.bsky.feed.post'
 GROUP BY user_id
 ORDER BY activity_span DESC
-LIMIT 3;
-
-------------------------------------------------------------------------------------------------------------------------
--- Q5 - (self-)join query - top 3 most liked posts
-------------------------------------------------------------------------------------------------------------------------
-WITH
-    T1 AS (
-        SELECT
-            data.commit.record.subject.cid::String as post_id,
-            count() as likes
-        FROM bluesky
-        WHERE data.kind = 'commit'
-          AND data.commit.operation = 'create'
-          AND data.commit.collection = 'app.bsky.feed.like'
-        GROUP BY post_id
-    ),
-    T2 AS (
-        SELECT
-            data.commit.cid::String as post_id,
-            data.commit.record.text::String as post_text
-        FROM bluesky
-        WHERE data.kind = 'commit'
-          AND data.commit.operation = 'create'
-          AND data.commit.collection = 'app.bsky.feed.post'
-    )
-SELECT
-    t1.likes,
-    t2.post_text
-FROM T1 t1 INNER JOIN T2 t2 ON t1.post_id = t2.post_id
-ORDER BY likes DESC
 LIMIT 3;
