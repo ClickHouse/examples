5bf0454ab55efd4b03af5b23b77627d83646c110
Tom Schreiber <tom@clickhouse.com>
2025-08-14 08:12:34 +0100

    Refactored for UPDATEBench instead of tests of ClickHouse against itself.

--- blog-examples/UPDATEBench/clickhouse/scripts/single-row-updates/10x1/lightweight_update_02.sql
+++
@@ -1,3 +0,0 @@
-UPDATE lineitem
-SET l_discount = 0.015, l_tax = 0.03, l_extendedprice = 432.10, l_returnflag = 'B', l_linestatus = 'X', l_comment = 'Adjusted manually'
-WHERE l_orderkey = 522639521 AND l_linenumber = 3;
--- blog-examples/UPDATEBench/clickhouse/scripts/single-row-updates/10x1/lightweight_update_05.sql
+++
@@ -1,3 +0,0 @@
-UPDATE lineitem
-SET l_comment = 'Backfill historical', l_discount = 0.01, l_tax = 0.09
-WHERE l_orderkey = 93311302 AND l_linenumber = 2;
